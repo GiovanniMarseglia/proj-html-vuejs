9b9c40f30090eb3e8901aab30adae984a606304e
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-02-19 19:28:44 +0100

    added other service

--- src/components/main/otherservice.vue
+++ src/components/main/otherservice.vue
@@ -12,11 +12,19 @@
 
 
 <template>
-    <div>
+    <div class="container-fluid py-5">
+        <div class="container mx-auto d-flex justify-content-between">
+            <img src="../../img/otherserviceimg/clients1.png" alt="">
+            <img src="../../img/otherserviceimg/clients2.png" alt="">
+            <img src="../../img/otherserviceimg/clients3.png" alt="">
+            <img src="../../img/otherserviceimg/clients4.png" alt="">
+        </div>
         
     </div>
 </template>
 
 <style lang="scss" scoped>
-
+    img{
+        width: 80px;
+    }
 </style>
