9cc5e631b92f7f4f724552a2079c225f029d3675
giovanni <63284349+GiovanniMarseglia@users.noreply.github.com>
2024-02-19 11:15:48 +0100

    Initial commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+
+<template>
+  <HelloWorld/>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+
+</style>
